4d39797578967eda96edb13b1c37c21733b7f426
Lyndsey <lyndsey@lyndseyjane.com>
2013-12-03 15:32:11 -0500

    Added support for toggling pause/play
    Added pause, play, stop, and update functions to help manage the SoundManager2 object

--- src/lightning_browse/core.cljs
+++ src/lightning_browse/core.cljs
@@ -3,11 +3,31 @@
             [lightning-browse.utils :as utils]
             [cljs.core.async :as async :refer [chan close! sliding-buffer put! >! <!]])
   (:require-macros
-   [cljs.core.async.macros :refer [go]]))
+   [cljs.core.async.macros :refer [go go-loop]]))
+
+(def current-track (atom nil))
+;; three functions: stop current track, play current track, update current track
+(defn stop-current-track! []
+  (when @current-track
+    (.stop @current-track)))
+
+(defn play-current-track! []
+  (when @current-track
+    (.play @current-track)))
+
+(defn update-current-track! [track]
+  (stop-current-track!)
+  (reset! current-track track))
+
+(defn pause-current-track! []
+  (when @current-track
+    (.pause @current-track)))
 
 (defn stream-track [track]
   (. js/SC (stream (str "/tracks/" track)
-                   (fn [sound] (.play sound)))))
+                   (fn [sound]
+                     (update-current-track! sound)
+                     (play-current-track!)))))
 
 (defn get-tracks [limit]
   (let [tracks (chan 1)]
@@ -30,11 +50,15 @@
 (. js/SC (initialize (clj->js config/settings)))
 
 (defn -main []
-(def queue (get-tracks 1000))
+  (let [queue (get-tracks 100)
+        click-events (event-chan (.getElementById js/document "play") "click")
+        transition {:init :playing, :paused :playing, :playing :paused}]
+    (go-loop [state :init]
+       (<! click-events)
+       (case state
+         :init (stream-track ((<! queue) "id"))
+         :paused (play-current-track!)
+         :playing (pause-current-track!))
+       (recur (transition state)))))
 
-  (go
-   (let [click-events (event-chan (.getElementById js/document "play") "click")]
-     (<! click-events)
-     (stream-track ((<! queue) "id")))))
-
-(set! (.-onload js/window) -main)
+  (set! (.-onload js/window) -main)
